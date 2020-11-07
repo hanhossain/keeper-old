9cebb3c9a2678a6255b40d4994419a68c9c61123
Han Hossain <ahanuf12@gmail.com>
2020-11-07 02:08:38 -0700

    sort by sections

--- ios/Keeper/PlayersTableViewController.swift
+++ ios/Keeper/PlayersTableViewController.swift
@@ -10,14 +10,32 @@ import UIKit
 class PlayersTableViewController: UITableViewController {
     
     let cellId = "player"
-    var players = [Player]()
+    var players = [Character : [Player]]()
+    var sections = [Character]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let playerService = PlayerService()
         playerService.getPlayers { (players) in
-            self.players = players
+            
+            for player in players {
+                var firstLetter = player.lastName.first!
+                if firstLetter.isNumber {
+                    firstLetter = "#"
+                }
+                
+                if self.sections.last != firstLetter {
+                    self.sections.append(firstLetter)
+                }
+                
+                if var grouped = self.players[firstLetter] {
+                    grouped.append(player)
+                    self.players[firstLetter] = grouped
+                } else {
+                    self.players[firstLetter] = [player]
+                }
+            }
             
             DispatchQueue.main.async {
                 self.tableView.reloadData()
@@ -27,61 +45,23 @@ class PlayersTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    /*
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return sections.count
     }
-    */
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return players.count
+        return players[sections[section]]?.count ?? 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
         
-        let player = players[indexPath.row]
-        cell.textLabel?.text = player.name
+        let player = players[sections[indexPath.section]]?[indexPath.row]
+        cell.textLabel?.text = player?.name
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
