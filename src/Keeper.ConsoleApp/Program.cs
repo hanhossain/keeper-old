2d67c411d03b9eff0b38113332ffeafbe7078ec6
Han Hossain <ahanuf12@gmail.com>
2021-08-31 21:23:04 -0700

    add console app

---
+++ src/Keeper.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Keeper.Core.Services;
+
+namespace Keeper.ConsoleApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var playerService = new PlayerService();
+            var players = await playerService.GetPlayersAsync();
+            Console.WriteLine($"{players.Count} players");
+
+            var player = players.First();
+            var json = JsonSerializer.Serialize(player);
+            Console.WriteLine(json);
+        }
+    }
+}
