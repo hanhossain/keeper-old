a323227c473ac5b07c0e639f2c1664991342523d
Han Hossain <ahanuf12@gmail.com>
2021-10-14 23:55:29 -0700

    only keep Keeper.Core for now

---
+++ src/Keeper.Core/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Keeper.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
