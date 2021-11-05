b4b0a8d1bcdfe5a7820e8dd508ea5a19a40481f9
Han Hossain <ahanuf12@gmail.com>
2021-11-04 20:48:02 -0700

    format file

--- src/Keeper.Core/Program.cs
+++ src/Keeper.Core/Program.cs
@@ -25,101 +25,101 @@ namespace Keeper.Core
         private static async Task UpdateNflAsync()
         {
             using var httpClient = new HttpClient();
-                var fantasyClient = new FantasyClient(httpClient);
+            var fantasyClient = new FantasyClient(httpClient);
 
-                var tasks = new[]
+            var tasks = new[]
+            {
+                fantasyClient.GetAsync(2021, NflPosition.Quarterback),
+                fantasyClient.GetAsync(2021, NflPosition.RunningBack),
+                fantasyClient.GetAsync(2021, NflPosition.WideReceiver),
+                fantasyClient.GetAsync(2021, NflPosition.TightEnd),
+                fantasyClient.GetAsync(2021, NflPosition.Kicker),
+                fantasyClient.GetAsync(2021, NflPosition.Defense)
+            };
+
+            var results = await Task.WhenAll(tasks);
+
+            var databaseTasks = results
+                .SelectMany(x => x)
+                .SelectMany(x => x.Values)
+                .Select(async x =>
                 {
-                    fantasyClient.GetAsync(2021, NflPosition.Quarterback),
-                    fantasyClient.GetAsync(2021, NflPosition.RunningBack),
-                    fantasyClient.GetAsync(2021, NflPosition.WideReceiver),
-                    fantasyClient.GetAsync(2021, NflPosition.TightEnd),
-                    fantasyClient.GetAsync(2021, NflPosition.Kicker),
-                    fantasyClient.GetAsync(2021, NflPosition.Defense)
-                };
+                    await using var dbContext = new DatabaseContext();
 
-                var results = await Task.WhenAll(tasks);
-
-                var databaseTasks = results
-                    .SelectMany(x => x)
-                    .SelectMany(x => x.Values)
-                    .Select(async x =>
+                    if (!await dbContext.NflPlayers.AnyAsync(p => p.Id == x.Id))
                     {
-                        await using var dbContext = new DatabaseContext();
-
-                        if (!await dbContext.NflPlayers.AnyAsync(p => p.Id == x.Id))
+                        var player = new NflPlayer()
                         {
-                            var player = new NflPlayer()
-                            {
-                                Id = x.Id,
-                                Name = x.Name,
-                                Position = x.Position,
-                                Team = x.Team?.Name
-                            };
+                            Id = x.Id,
+                            Name = x.Name,
+                            Position = x.Position,
+                            Team = x.Team?.Name
+                        };
 
-                            dbContext.NflPlayers.Add(player);
-                            await dbContext.SaveChangesAsync();
-                        }
+                        dbContext.NflPlayers.Add(player);
+                        await dbContext.SaveChangesAsync();
+                    }
 
-                        if (!await dbContext.NflPlayerStatistics.AnyAsync(p =>
-                            p.PlayerId == x.Id && p.Season == x.Season &&
-                            p.Week == x.Week))
+                    if (!await dbContext.NflPlayerStatistics.AnyAsync(p =>
+                        p.PlayerId == x.Id && p.Season == x.Season &&
+                        p.Week == x.Week))
+                    {
+                        var statistics = new NflPlayerStatistics()
                         {
-                            var statistics = new NflPlayerStatistics()
-                            {
-                                PlayerId = x.Id,
-                                Season = x.Season,
-                                Week = x.Week,
-                                FantasyPoints = x.Statistics.FantasyPoints
-                            };
+                            PlayerId = x.Id,
+                            Season = x.Season,
+                            Week = x.Week,
+                            FantasyPoints = x.Statistics.FantasyPoints
+                        };
 
-                            dbContext.NflPlayerStatistics.Add(statistics);
-                            await dbContext.SaveChangesAsync();
-                        }
+                        dbContext.NflPlayerStatistics.Add(statistics);
+                        await dbContext.SaveChangesAsync();
+                    }
 
-                        if (x.Statistics.Kicking != null && !await dbContext.NflKickingStatistics.AnyAsync(p =>
-                            p.PlayerId == x.Id && p.Season == x.Season && p.Week == x.Week))
+                    if (x.Statistics.Kicking != null && !await dbContext.NflKickingStatistics.AnyAsync(p =>
+                        p.PlayerId == x.Id && p.Season == x.Season && p.Week == x.Week))
+                    {
+                        var kickingStatistics = new NflKickingStatistics()
                         {
-                            var kickingStatistics = new NflKickingStatistics()
-                            {
-                                PlayerId = x.Id,
-                                Season = x.Season,
-                                Week = x.Week,
-                                PatMade = x.Statistics.Kicking.Pat.Made,
-                                FieldGoal0To19Yards = x.Statistics.Kicking.FieldGoal.Yards0To19,
-                                FieldGoal20To29Yards = x.Statistics.Kicking.FieldGoal.Yards20To29,
-                                FieldGoal30To39Yards = x.Statistics.Kicking.FieldGoal.Yards30To39,
-                                FieldGoal40To49Yards = x.Statistics.Kicking.FieldGoal.Yards40To49,
-                                FieldGoal50PlusYards = x.Statistics.Kicking.FieldGoal.Yards50Plus
-                            };
+                            PlayerId = x.Id,
+                            Season = x.Season,
+                            Week = x.Week,
+                            PatMade = x.Statistics.Kicking.Pat.Made,
+                            FieldGoal0To19Yards = x.Statistics.Kicking.FieldGoal.Yards0To19,
+                            FieldGoal20To29Yards = x.Statistics.Kicking.FieldGoal.Yards20To29,
+                            FieldGoal30To39Yards = x.Statistics.Kicking.FieldGoal.Yards30To39,
+                            FieldGoal40To49Yards = x.Statistics.Kicking.FieldGoal.Yards40To49,
+                            FieldGoal50PlusYards = x.Statistics.Kicking.FieldGoal.Yards50Plus
+                        };
 
-                            dbContext.NflKickingStatistics.Add(kickingStatistics);
-                            await dbContext.SaveChangesAsync();
-                        }
+                        dbContext.NflKickingStatistics.Add(kickingStatistics);
+                        await dbContext.SaveChangesAsync();
+                    }
 
-                        if (x.Statistics.Defensive != null && !await dbContext.NflDefensiveStatistics.AnyAsync(p =>
-                            p.PlayerId == x.Id && p.Season == x.Season && p.Week == x.Week))
+                    if (x.Statistics.Defensive != null && !await dbContext.NflDefensiveStatistics.AnyAsync(p =>
+                        p.PlayerId == x.Id && p.Season == x.Season && p.Week == x.Week))
+                    {
+                        var defensiveStatistics = new NflDefensiveStatistics()
                         {
-                            var defensiveStatistics = new NflDefensiveStatistics()
-                            {
-                                PlayerId = x.Id,
-                                Season = x.Season,
-                                Week = x.Week,
-                                Sacks = x.Statistics.Defensive.Tackling.Sacks,
-                                Interceptions = x.Statistics.Defensive.Turnover.Interceptions,
-                                FumblesRecovered = x.Statistics.Defensive.Turnover.FumblesRecovered,
-                                Safeties = x.Statistics.Defensive.Score.Safeties,
-                                Touchdowns = x.Statistics.Defensive.Score.Touchdowns,
-                                Def2PtRet = x.Statistics.Defensive.Score.Def2PtRet,
-                                RetTouchdowns = x.Statistics.Defensive.Returning.Touchdowns,
-                                PointsAllowed = x.Statistics.Defensive.Points.PointsAllowed
-                            };
+                            PlayerId = x.Id,
+                            Season = x.Season,
+                            Week = x.Week,
+                            Sacks = x.Statistics.Defensive.Tackling.Sacks,
+                            Interceptions = x.Statistics.Defensive.Turnover.Interceptions,
+                            FumblesRecovered = x.Statistics.Defensive.Turnover.FumblesRecovered,
+                            Safeties = x.Statistics.Defensive.Score.Safeties,
+                            Touchdowns = x.Statistics.Defensive.Score.Touchdowns,
+                            Def2PtRet = x.Statistics.Defensive.Score.Def2PtRet,
+                            RetTouchdowns = x.Statistics.Defensive.Returning.Touchdowns,
+                            PointsAllowed = x.Statistics.Defensive.Points.PointsAllowed
+                        };
 
-                            dbContext.NflDefensiveStatistics.Add(defensiveStatistics);
-                            await dbContext.SaveChangesAsync();
-                        }
-                    });
+                        dbContext.NflDefensiveStatistics.Add(defensiveStatistics);
+                        await dbContext.SaveChangesAsync();
+                    }
+                });
 
-                await Task.WhenAll(databaseTasks);
+            await Task.WhenAll(databaseTasks);
         }
 
         private static async Task MigrateDatabaseAsync()
