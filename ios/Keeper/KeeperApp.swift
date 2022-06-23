a4ab61f5565b94000450776deaf7758abba172e6
Han Hossain <ahanuf12@gmail.com>
2022-06-23 10:45:03 -0700

    initial swift ui commit

---
+++ ios/Keeper/KeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeeperApp.swift
+//  Keeper
+//
+//  Created by Han Hossain on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct KeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
