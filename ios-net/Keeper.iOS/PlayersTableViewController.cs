1084b49ba084efd6e23fcf79108c179a654b4895
Han Hossain <ahanuf12@gmail.com>
2022-06-25 21:22:01 -0700

    section headers

--- ios-net/Keeper.iOS/PlayersTableViewController.cs
+++ ios-net/Keeper.iOS/PlayersTableViewController.cs
@@ -15,7 +15,9 @@ public class PlayersTableViewController : UITableViewController
     private const string CellId = nameof(PlayersTableViewController);
 
     private readonly SleeperClient _sleeperClient;
-    private List<SleeperPlayer> _players = new List<SleeperPlayer>();
+
+    private List<char> _sectionHeaders = new List<char>();
+    private Dictionary<char, List<SleeperPlayer>> _players = new Dictionary<char, List<SleeperPlayer>>();
 
     public PlayersTableViewController(HttpClient httpClient)
     {
@@ -34,26 +36,44 @@ public class PlayersTableViewController : UITableViewController
         _players = players
             .Values
             .Where(x => x.Active && validPositions.Contains(x.Position))
-            .OrderBy(x => x.LastName)
-            .ThenBy(x => x.FirstName)
-            .ToList();
+            .GroupBy(x =>
+            {
+                var firstLetter = x.LastName.First();
+                return char.IsNumber(firstLetter) ? '#' : firstLetter;
+            })
+            .ToDictionary(x => x.Key, x => x.ToList());
+
+        _sectionHeaders = _players.Keys.OrderBy(x => x).ToList();
 
         TableView.ReloadData();
     }
 
+    public override nint NumberOfSections(UITableView tableView)
+    {
+        return _sectionHeaders.Count;
+    }
+
     public override nint RowsInSection(UITableView tableView, nint section)
     {
-        return _players.Count;
+        var sectionHeader = _sectionHeaders[(int)section];
+        return _players[sectionHeader].Count;
     }
 
     public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
     {
         var cell = tableView.DequeueReusableCell<SubtitleTableViewCell>(CellId, indexPath);
 
-        var player = _players[indexPath.Row];
+        var sectionHeader = _sectionHeaders[indexPath.Section];
+        var player = _players[sectionHeader][indexPath.Row];
+
         cell.TextLabel.Text = $"{player.FirstName} {player.LastName}";
         cell.DetailTextLabel.Text = $"{player.Position} - {player.Team}";
 
         return cell;
+    }
+
+    public override string[] SectionIndexTitles(UITableView tableView)
+    {
+        return _sectionHeaders.Select(x => x.ToString()).ToArray();
     }
 }
