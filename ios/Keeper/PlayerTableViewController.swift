36f1ddbf2db71a408fef2254f5334502e685aa68
Han Hossain <ahanuf12@gmail.com>
2022-07-01 00:53:07 -0700

    native search controller

--- ios/Keeper/PlayerTableViewController.swift
+++ ios/Keeper/PlayerTableViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class PlayerTableViewController: UITableViewController {
-    private var sections = [Character]()
+class PlayerTableViewController: UITableViewController, UISearchResultsUpdating {
+    private var sectionHeaders = [Character]()
     private var players = [Character: [Player]]()
+    private var filteredPlayers = [Character: [Player]]()
     private var seasonStatistics = [String: SeasonStatistics]()
     
     private let cellId = "playerCell"
@@ -17,8 +18,12 @@ class PlayerTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.register(SubtitleRightDetailTableViewCell.self, forCellReuseIdentifier: cellId)
         title = "Players"
+        tableView.register(SubtitleRightDetailTableViewCell.self, forCellReuseIdentifier: cellId)
+        
+        let searchController = UISearchController()
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
         
         Task {
             async let players: () = loadPlayers()
@@ -31,7 +36,7 @@ class PlayerTableViewController: UITableViewController {
         }
     }
     
-    func getSeasonStatistics() async {
+    private func getSeasonStatistics() async {
         do {
             let seasonStatistics = try await sleeperClient.getSeasonStatistics()
             
@@ -46,43 +51,53 @@ class PlayerTableViewController: UITableViewController {
         }
     }
     
-    func loadPlayers() async {
+    private func loadPlayers() async {
         do {
             let allPlayers = try await sleeperClient.getPlayers()
             
             let validPositions = Set(["QB", "RB", "WR", "TE", "K", "DEF"])
-            let filteredPlayers = allPlayers.values
-                .filter { $0.active && validPositions.contains($0.position ?? "") }
-                .sorted { player1, player2 in
-                    if player1.lastName == player2.lastName {
-                        return player1.firstName < player2.firstName
-                    } else {
-                        return player1.lastName < player2.lastName
-                    }
-                }
+            let filteredPlayers = allPlayers.values.filter { $0.active && validPositions.contains($0.position ?? "") }
 
-            players = Dictionary(grouping: filteredPlayers, by: { $0.lastName.first! })
-            sections = players.keys.sorted()
+            players = processPlayers(filteredPlayers)
+            filterPlayers(players)
         } catch {
             print("Failed to get players: \(error)")
         }
+    }
+    
+    private func processPlayers(_ players: [Player]) -> [Character: [Player]] {
+        let sortedPlayers = players.sorted { player1, player2 in
+            if player1.lastName == player2.lastName {
+                return player1.firstName < player2.firstName
+            } else {
+                return player1.lastName < player2.lastName
+            }
+        }
+        
+        return Dictionary(grouping: sortedPlayers, by: { $0.lastName.first! })
+    }
+    
+    private func filterPlayers(_ players: [Character: [Player]]) {
+        filteredPlayers = players
+        sectionHeaders = players.keys.sorted()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        sections.count
+        sectionHeaders.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        players[sections[section]]!.count
+        let sectionHeader = sectionHeaders[section]
+        return filteredPlayers[sectionHeader]!.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! SubtitleRightDetailTableViewCell
 
-        let section = sections[indexPath.section]
-        let player = players[section]![indexPath.row]
+        let sectionHeader = sectionHeaders[indexPath.section]
+        let player = filteredPlayers[sectionHeader]![indexPath.row]
         let statistics = seasonStatistics[player.playerId]
 
         cell.mainLabel.text = "\(player.firstName) \(player.lastName)"
@@ -96,6 +111,22 @@ class PlayerTableViewController: UITableViewController {
     }
     
     override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        sections.map { $0.isLetter ? String($0) : "#" }
+        sectionHeaders.map { $0.isLetter ? String($0) : "#" }
+    }
+    
+    // MARK: - UISearchResultsUpdating
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let query = searchController.searchBar.text, !query.isEmpty {
+            let playerQuery = players.values
+                .flatMap { $0 }
+                .filter { "\($0.firstName) \($0.lastName)".localizedCaseInsensitiveContains(query) }
+            let processedPlayers = processPlayers(playerQuery)
+            filterPlayers(processedPlayers)
+        } else {
+            filterPlayers(players)
+        }
+        
+        tableView.reloadData()
     }
 }
